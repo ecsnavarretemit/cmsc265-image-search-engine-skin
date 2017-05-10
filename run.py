7f66080accf5c45121ee5764cabe1e6ba7f97e5c
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-05-10 12:03:52 +0800

    Implement run script for application

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # run.py
 #
 # Author(s):
@@ -5,5 +7,11 @@
 #
 # Licensed under MIT
 # Version 1.0.0-alpha1
+
+from app import app
+
+# run the application
+if __name__ == "__main__":
+  app.run()
 
 
