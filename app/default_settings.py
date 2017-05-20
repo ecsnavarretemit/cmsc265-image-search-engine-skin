243ccbd625cbd32bc740cfd7b53f0455508134e2
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-05-20 11:26:28 +0800

    Show message when there are no processed images available yet

--- app/default_settings.py
+++ app/default_settings.py
@@ -29,6 +29,12 @@ CACHE_DIRECTORY = os.path.join(os.getcwd(), "data/cache")
 
 APP_TITLE = "CMSC 265 Image Search Engine for Skin"
 
+SKIN_DETECT_CACHE_KEY = "detected_skins"
+SKIN_DETECT_INPUT_DIR = os.path.join(os.getcwd(), "assets/img/input-for-skin-detection")
+SKIN_DETECT_OUTPUT_DIR = os.path.join(os.getcwd(), "app/static/img/detected-skins")
+SKIN_DETECT_IM_WIDTH = 800
+SKIN_DETECT_IM_HEIGHT = 450
+
 #========================================================
 # [Application-specific configuration] ::end
 #========================================================
--- app/views/home.py
+++ app/views/home.py
@@ -9,9 +9,11 @@
 import os
 from app import app, cache
 from app.utils import get_images
+from app.utils.exceptions import DirectoryNotFoundException
 from flask import Blueprint, render_template, request
 from flask_paginate import Pagination
 
+# setup blueprint
 home = Blueprint('home', __name__, url_prefix='/')
 
 #===================================================
@@ -20,7 +22,8 @@ home = Blueprint('home', __name__, url_prefix='/')
 
 @home.route('/', methods=['GET'])
 def index():
-  cache_key = "detected_skins"
+  cache_key = app.config['SKIN_DETECT_CACHE_KEY']
+  message = ""
 
   page = request.args.get('page')
   num_images_per_page = 50
@@ -35,35 +38,40 @@ def index():
   images_meta = cache.get(cache_key)
 
   # if images is not on the cache, fetch all images from the directory
-  # and cache it for 1 hour
+  # and cache it for 15 minutes
   if images_meta is None:
-    stored_images = get_images(skins_folder)
+    # set default empty value to the fetch images
+    stored_images = []
+
+    try:
+      stored_images = get_images(skins_folder)
+    except DirectoryNotFoundException:
+      pass
 
     images = []
     for image in stored_images:
+      # trim the path of the image
       if image.startswith(app.root_path):
         images.append(image[len(app.root_path):])
 
     # do something if the number of found images is 0
     num_images = len(images)
-    if num_images == 0:
-      pass
+    if num_images > 0:
+      # sort the images based on the percentage
+      images.sort(key=extract_percentage, cmp=lambda x, y: int(x - y))
 
-    # sort the images based on the percentage
-    images.sort(key=extract_percentage, cmp=lambda x, y: int(x - y))
+      # reverse the ordering
+      images = list(reversed(images))
 
-    # reverse the ordering
-    images = list(reversed(images))
+      # convert the list of paths to list of dictionaries containing image metadata
+      images = map(lambda x: { # pylint: disable=W0110
+        'path_normal': x,
+        'path_thumb': x,
+        'percent': extract_percentage(x)
+      }, images)
 
-    # convert the list of paths to list of dictionaries containing image metadata
-    images = map(lambda x: { # pylint: disable=W0110
-      'path_normal': x,
-      'path_thumb': x,
-      'percent': extract_percentage(x)
-    }, images)
-
-    # split the image 50 per item
-    images = [images[i:(i + num_images_per_page)] for i in range(0, num_images, num_images_per_page)]
+      # split the image 50 per item
+      images = [images[i:(i + num_images_per_page)] for i in range(0, num_images, num_images_per_page)]
 
     # save the metadata to the dictionary
     images_meta = {
@@ -71,17 +79,23 @@ def index():
       'total': num_images
     }
 
-    # cache it for 1 hour
-    cache.set(cache_key, images_meta, timeout=3600)
+    # cache it for 15 minutes
+    cache.set(cache_key, images_meta, timeout=900)
 
   # get the number of pages in the pagination
   num_pages = len(images_meta['items'])
 
+  # default value for the image subset
+  image_subset = []
+
   # get the subset of the images to be displayed
-  try:
-    image_subset = images_meta['items'][abs(page - 1)]
-  except IndexError:
-    image_subset = images_meta['items'][num_pages - 1]
+  if images_meta['total'] > 0:
+    try:
+      image_subset = images_meta['items'][abs(page - 1)]
+    except IndexError:
+      image_subset = images_meta['items'][num_pages - 1]
+  else:
+    message = "No images available to be displayed. Please try again later"
 
   # setup pagination
   pagination = Pagination(page=page,
@@ -91,7 +105,11 @@ def index():
                           bs_version=3,
                           css_framework='bootstrap')
 
-  return render_template('home/index.html', images=image_subset, num_pages=num_pages, pagination=pagination)
+  return render_template('home/index.html',
+                         message=message,
+                         images=image_subset,
+                         num_pages=num_pages,
+                         pagination=pagination)
 
 #===================================================
 # [Views] ::end
